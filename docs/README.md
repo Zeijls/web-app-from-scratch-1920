576b2067a2846cefed039e98fbf068b51705f55e
Simone van Zeijl <simonev.zeijl@hotmail.nl>
2020-02-21 13:50:30 +0100

    update

---
+++ docs/README.md
@@ -0,0 +1,74 @@
+# Web App From Scratch @cmda-minor-web 1819
+
+[Live Demo](https://zeijls.github.io/web-app-from-scratch-1920/);
+
+![Front-page](img/frontpage.png)
+
+## Inhoudspagina
+
+- Actor Diagram
+- Interactive Diagram
+- Leerdoelen
+- Beschrijving
+- API
+- Bronnen
+
+## Leerdoelen
+
+- You can add structure to your code by applying patterns.
+- You can defend the choice for the chosen patterns
+- You can retrieve data, manipulate it and dynamically convert it to html elements using templating
+- You understand how you can work with an external API using asynchronous code
+- You understand how you can manage state in your application and you inform the user of state where necessary
+
+## Beschrijving
+
+Op de webapplicatie zijn de verschillende schilderijen van het rijksmuseum weergegeven. De titel wordt in een hover weergegeven. Zodra je op een schilderij klikt worden de kleuren die in het schilderij zitten weergegeven. Daarnaast kun je zien waar het schilderij is gemaakt en de schilder.
+
+## API
+
+In deze applicatie gebruik ik de API van het Rijksmuseum. In deze API is een groot deel van de collectie van het Rijksmuseum verzameld. Alle details van de schilerderijen worden hierin weergegeven. Vanwege copyright restricties zijn kunstwerken van de 20e en 21e eeuw niet toegevoegd in deze API.
+
+Om gebruik te maken van de API van het Rijksmuseum heeft men een key nodig. Deze kan men aanvragen bij de gevanceerde account instellingen op de site van het Rijksumseum. https://www.rijksmuseum.nl/en/rijksstudio/
+
+## How to install
+
+## Features
+
+## External data source
+
+## Check list done stuff
+
+## Wishlist
+
+## License
+
+## Actor Diagram
+
+![Actor Diagram](./docs/img/actordiagram.png)
+
+## Interactive Diagram
+
+![Interactive Diagram](./docs/img/interactivediagram.png)
+
+## Bronnen
+
+<!-- Add a link to your live demo in Github Pages 🌐-->
+
+<!-- ☝️ replace this description with a description of your own work -->
+
+<!-- replace the code in the /docs folder with your own, so you can showcase your work with GitHub Pages 🌍 -->
+
+<!-- Add a nice poster image here at the end of the week, showing off your shiny frontend 📸 -->
+
+<!-- Maybe a table of contents here? 📚 -->
+
+<!-- How about a section that describes how to install this project? 🤓 -->
+
+<!-- ...but how does one use this project? What are its features 🤔 -->
+
+<!-- What external data source is featured in your project and what are its properties 🌠 -->
+
+<!-- Maybe a checklist of done stuff and stuff still on your wishlist? ✅ -->
+
+<!-- How about a license here? 📜 (or is it a licence?) 🤷 -->
--- docs/js/app.js
+++ docs/js/app.js
@@ -1,66 +1,108 @@
 const rijksAPI =
   "https://www.rijksmuseum.nl/api/nl/collection?key=uc0ncT1V&involvedMaker=Rembrandt+van+Rijn";
 const rijksContainer = document.getElementById("rijksContainer");
+const painting = document.getElementById("painting");
 /*** Handle routes -> refactor into module later ***/
 const sections = document.querySelectorAll("section");
+let data = null;
 
 // Fetch
 fetch(rijksAPI)
   .then(response => {
     return response.json();
   })
-
   .then(myJson => {
     console.log(myJson);
-    render(myJson);
+    data = myJson;
+    init(data);
   });
+function init(data) {
+  const activeSection = document.querySelector("[data-route=painting]");
+  console.log(activeSection);
+  function render(rijksData) {
+    // console.log(rijksData);
+    // console.log(rijksContainer);
+    let i = 0;
+    data.artObjects.map(function(x) {
+      rijksContainer.insertAdjacentHTML(
+        "beforeend",
+        `
+     <li>
+        <a href="#painting-${x.id}" > <img src=${x.webImage.url} alt=""> </a>
+     </li>`
+      );
+      i++;
+    });
+  }
 
-function render(rijksData) {
-  console.log(rijksData);
-  console.log(rijksContainer);
-
-  rijksData.artObjects.map(function(x) {
-    rijksContainer.insertAdjacentHTML(
+  function renderDetail(id) {
+    console.log(data);
+    const painting = data.artObjects
+      .filter(obj => obj.id == id)
+      .reduce(obj => obj);
+    console.log(painting);
+    activeSection.insertAdjacentHTML(
       "beforeend",
       `
-       <li>
-           <img src=${x.webImage.url} alt="">
-       
-       </li>`
+    <img src=${painting.webImage.url} alt="">
+    <div>
+    <section>${painting.title}</section>
+    <section>${painting.productionPlaces}</section>
+    <a href=""> Terug naar overzicht</a>
+    </div>
+    `
     );
-  });
-}
-
-// Titel en productieplaats
-// <section>${x.title}</section>
-// <section>${x.productionPlaces}</section>
-
-// Routen
-routie({
-  vanGogh: () => {
-    updateUI("vanGogh");
-  },
-  RembrandtvanRijn: () => {
-    updateUI("RembrandtvanRijn");
-  },
-  Top: () => {
-    removeTopBar();
+    // activeSection.insertAdjacentHTML(
+    //   "beforeend",
+    //   `
+    //   <li>
+    //   <img src=${x.webImage.url} alt="">
+    //   <section>${x.title}</section>
+    //   <section>${x.productionPlaces}</section>
+    //   </li>`
+    // );
   }
-});
+  // Titel en productieplaats
+  // <section>${x.title}</section>
+  // <section>${x.productionPlaces}</section>
 
-// https://github.com/cmda-minor-web/web-app-from-scratch-1920/tree/master/examples
-// update UI from route (hashchange)
-
-// Class toevoegen en verwijderen
-function updateUI(route) {
-  removeTopBar();
-  activeSection = document.querySelector(`[data-route=${route}]`);
-  console.log(activeSection);
-  activeSection.classList.add("active");
-}
-
-function removeTopBar() {
-  sections.forEach(section => {
-    section.classList.remove("active");
+  // Routen
+  routie({
+    "": () => {
+      render(data);
+    },
+    // vanGogh: () => {
+    //   updateUI("vanGogh");
+    // },
+    // RembrandtvanRijn: () => {
+    //   updateUI("RembrandtvanRijn");
+    // },
+    "painting-:id": id => {
+      console.log(id);
+      updateUI(id);
+    },
+    Top: () => {
+      removeTopBar();
+    }
   });
+
+  // https://github.com/cmda-minor-web/web-app-from-scratch-1920/tree/master/examples
+  // update UI from route (hashchange)
+  // Class toevoegen en verwijderen
+  function updateUI(route) {
+    removeTopBar();
+    document.querySelector(".wrapper").remove();
+
+    console.log(document.querySelector(".wrapper"));
+    // const activeSection = document.querySelector(`[data-route=${route}]`);
+    // console.log(activeSection);
+    activeSection.classList.add("active");
+    renderDetail(route);
+  }
+  function removeTopBar() {
+    activeSection.innerHTML = "";
+    sections.forEach(section => {
+      section.classList.remove("active");
+    });
+  }
 }
